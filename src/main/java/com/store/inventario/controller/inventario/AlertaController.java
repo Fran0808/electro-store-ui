6870267eacd8622fed791727f0092824eb48264e
Fran0808 <fortegaquipse@gmail.com>
2026-05-23 12:11:19 -0500

    feat: añadiendo controladores para alerta

---
+++ src/main/java/com/store/inventario/controller/inventario/AlertaController.java
@@ -0,0 +1,23 @@
+package com.store.inventario.controller.inventario;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AlertaController {
+    @FXML
+    private void handleForm() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/store/inventario/views/inventario/alertas-form.fxml"));
+        Parent root = loader.load();
+        Stage modal = new Stage();
+        modal.initModality(Modality.APPLICATION_MODAL);
+        modal.setTitle("Editar límite de alertas");
+        modal.setScene(new Scene(root));
+        modal.showAndWait();
+    }
+}
