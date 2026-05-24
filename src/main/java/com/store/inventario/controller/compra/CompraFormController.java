ecfb0e75acb0860d73594d9decff7d6d7c5b7dd5
Cesar1503 <uscuvilcacesar88@gmail.com>
2026-05-24 18:30:49 -0500

    feat: Funcionamiento correcto del formulario nueva compra

--- src/main/java/com/store/inventario/controller/compra/CompraFormController.java
+++ src/main/java/com/store/inventario/controller/compra/CompraFormController.java
@@ -1,4 +1,18 @@
 package com.store.inventario.controller.compra;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class CompraFormController {
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private void cerrarModal() {
+        Stage stage = (Stage) btnCancelar.getScene().getWindow();
+        stage.close();
+    }
+
 }
