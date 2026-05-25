9797965898e4ca30698923fec40434659088118b
Cesar1503 <uscuvilcacesar88@gmail.com>
2026-05-24 19:02:14 -0500

    feat: Funcionamiento correcto del formulario agregar producto en formulario nueva compra

--- src/main/java/com/store/inventario/controller/compra/CompraFormController.java
+++ src/main/java/com/store/inventario/controller/compra/CompraFormController.java
@@ -1,8 +1,14 @@
 package com.store.inventario.controller.compra;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CompraFormController {
 
@@ -15,4 +21,14 @@ public class CompraFormController {
         stage.close();
     }
 
+    @FXML
+    public void abrirModalAgregarProductos() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/store/inventario/views/compras/view-agregarProducto-modal.fxml"));
+        Parent root = loader.load();
+        Stage modal = new Stage();
+        modal.initModality(Modality.APPLICATION_MODAL);
+        modal.setTitle("Nueva Compra");
+        modal.setScene(new Scene(root));
+        modal.showAndWait();
+    }
 }
