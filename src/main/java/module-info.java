52189fa0bf6b9d08289dca13c8104b29668c9757
Fran0808 <fortegaquipse@gmail.com>
2026-05-24 22:32:04 -0500

    feat: módulos abiertos para controladores

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,16 @@ module com.store.inventario {
     requires javafx.fxml;
 
 
-    opens com.store.inventario to javafx.fxml;
     exports com.store.inventario;
+    opens com.store.inventario to javafx.fxml;
+    opens com.store.inventario.controller.login to javafx.fxml;
+    opens com.store.inventario.controller.ventas to javafx.fxml;
+    opens com.store.inventario.controller.productos to javafx.fxml;
+    opens com.store.inventario.controller.inventario.alertas to javafx.fxml;
+    opens com.store.inventario.controller.inventario.guias to javafx.fxml;
+    opens com.store.inventario.controller.inventario.movimientos to javafx.fxml;
+    opens com.store.inventario.controller.compra to javafx.fxml;
+    opens com.store.inventario.controller.empleados to javafx.fxml;
+    opens com.store.inventario.controller.proveedor to javafx.fxml;
+
 }
