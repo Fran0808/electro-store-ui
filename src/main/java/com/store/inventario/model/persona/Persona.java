bc6375e8d0b389dcf15c64599738ae12a676b061
Fran0808 <fortegaquipse@gmail.com>
2026-05-30 00:26:41 -0500

    feat: creacion de modelos para persona

---
+++ src/main/java/com/store/inventario/model/persona/Persona.java
@@ -0,0 +1,60 @@
+package com.store.inventario.model.persona;
+
+public class Persona {
+    private String code;
+    private String firstName;
+    private String lastName;
+    private String nationalId;
+    private String phone;
+
+    public Persona() {
+    }
+
+    public Persona(String code, String firstName, String lastName, String nationalId, String phone) {
+        this.code = code;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nationalId = nationalId;
+        this.phone = phone;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public void setNationalId(String nationalId) {
+        this.nationalId = nationalId;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
